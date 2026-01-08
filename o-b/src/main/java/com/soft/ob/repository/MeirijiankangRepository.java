50ba588a1e20b983de385d42cf87b0a4d1b3fe67
Yneighbour <152376188+Yneighbour@users.noreply.github.com>
2026-01-08 10:34:43 +0800

    后端重做
    后端重做

--- o-b/src/main/java/com/soft/ob/repository/MeirijiankangRepository.java
+++
@@ -1,9 +0,0 @@
-package com.soft.ob.repository;
-
-import com.soft.ob.entity.Meirijiankang;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MeirijiankangRepository extends JpaRepository<Meirijiankang, Long> {
-}
